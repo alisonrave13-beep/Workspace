3f1f041a3a553cddf892f8c723d712e47b68e076
alisonrave13-beep <alisonrave13@gmail.com>
2026-03-24 21:10:43 +0000

    ejercicios

---
+++ 24-03/ejercicio01-1.js
@@ -0,0 +1,8 @@
+var nombre ="alison"
+let edad = 17
+const ciudad ="BOGOTA" 
+console.log(nombre);
+console.log("edad:",edad);
+console.log("ciudad:",ciudad);
+console.log("nombre:",nombre,"edad:",edad,"ciudad:",ciudad);
+console.log("chocolate:",nombre,"edad:",edad,"ciudad:",ciudad);
