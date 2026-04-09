d3a68f17e2b989ff2980fd18d9631ac4238cce90
alisonrave13-beep <alisonrave13@gmail.com>
2026-04-09 16:12:58 -0500

    Enhance db.sql with comments on table schemas
    Added comments to table definitions explaining data types.

--- db.sql
+++ db.sql
@@ -1,15 +1,25 @@
 create database cryptoguard;
 use cryptoguard;
-create table producto (   id int primary key, bitcoin varchar(120));
+create table producto (   
+  id int primary key, // es entero porque se guarda un numero 
+  bitcoin varchar(120)//es varcgar porque es letra);
 
-create table ventas( id int primary key, ProductosVendidos int,
-fechaDeVenta datetime, ValorDeLaVenta int, productoPorVender(120))
+create table ventas( id int primary key, // es entero porque se guarda un numero 
+  ProductosVendidos int,// es un numero porque es la cantidad de productos vendidos
+fechaDeVenta datetime,// es un numero porque es la fecha de las ventas
+  ValorDeLaVenta int,// es un numero porque es el valor de la venta 
+productoPorVender varchar(120)//es varchar porque es letra)
 
-create comprador(documento int primary key, PrimerNombre varchar(50),apellido varchar(50));
+create comprador(documento int primary key, // es entero porque se guarda un numero 
+  PrimerNombre varchar(50),//es varchar porque es letra
+  apellido varchar(50),//es varchar porque es letra);
 
-create table comentarios (id int primary key, comprador varchar(50),
-vendedor varchar(50), administrador varchar(50));
+create table comentarios (id int primary key, // es entero porque se guarda un numero 2
+  comprador varchar(50),//es varchar porque es letra
+vendedor varchar(50),//es varchar porque es letra
+  administrador varchar(50),//es varchar porque es letra)
 
-create table vendedor(documento int primary key,
-nombre varchar(50),apellido varchar(50))
+create table vendedor(documento int primary key, // es entero porque se guarda un numero 
+nombre varchar(50),//es varchar porque es letra
+apellido varchar(50),//es varchar porque es letra)
 
