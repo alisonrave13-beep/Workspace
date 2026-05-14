0e4101eec018deea34799b9c7f6e34776fbe6426
alisonrave13-beep <alisonrave13@gmail.com>
2026-05-14 19:36:12 +0000

    coco

---
+++ 14-05/calculadora-funciones/app.js
@@ -0,0 +1,66 @@
+function sumar(a, b) {
+    resultado=a+b;
+    return resultado;
+
+}
+
+function restar(a, b) {
+    resultado=a-b;
+    return resultado;
+
+}
+
+// Función multiplicación
+function multiplicar(a, b) {
+    resultado=a*b;
+    return resultado;
+
+}
+
+// Función división
+function dividir(a, b) {
+    resultado=a/b;
+    return resultado;
+
+}
+
+
+function potencia(a, b) {
+    resultado=a**b;
+    return resultado;
+
+   
+}
+
+function mayor(a, b) {
+    resultado=a>b;
+    return resultado;
+
+
+}
+console.log("Resultado potencia:");
+console.log(potencia(10, 5));
+console.log("Resultado mayor:");
+console.log(mayor(10, 12));
+
+// =====================================
+// LLAMADO DE FUNCIONES
+// =====================================
+
+console.log("Resultado suma:");
+console.log(sumar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado resta:");
+console.log(restar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado multiplicación:");
+console.log(multiplicar(10, 5));
+
+console.log("----------------");
+
+console.log("Resultado división:");
+console.log(dividir(10,0));
